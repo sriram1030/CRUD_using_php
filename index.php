d5cf4fc689a279eef1b1a3950ce6f0312f56ff8d
sriram1030 <sriram1030@gmail.com>
2025-02-10 16:08:44 +0530

    CRUD Completed

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!--<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -29,4 +29,4 @@
 
 <script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>    
 </body>
-</html>
+</html> !-->
